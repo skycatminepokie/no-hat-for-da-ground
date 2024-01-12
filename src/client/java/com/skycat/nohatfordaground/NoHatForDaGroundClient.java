d6d6e4e1ff4fb62a3c6555b0b32a094196fe312c
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-01-11 16:26:13 -0800

    Initial commit

---
+++ src/client/java/com/skycat/nohatfordaground/NoHatForDaGroundClient.java
@@ -0,0 +1,10 @@
+package com.skycat.nohatfordaground;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoHatForDaGroundClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
