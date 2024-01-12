fd7f2533e830ec6175e44cfae62e46edb5b97d63
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-01-11 17:18:58 -0800

    Might've done it?

--- src/main/java/com/skycat/nohatfordaground/NoHatForDaGround.java
+++ src/main/java/com/skycat/nohatfordaground/NoHatForDaGround.java
@@ -2,14 +2,34 @@ package com.skycat.nohatfordaground;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NoHatForDaGround implements ModInitializer {
+public class NoHatForDaGround implements ModInitializer, UseBlockCallback {
     public static final Logger LOGGER = LoggerFactory.getLogger("no-hat-for-da-ground");
 
 	@Override
-	public void onInitialize() {
+	public ActionResult interact(PlayerEntity player, World world, Hand hand, BlockHitResult hitResult) {
+		ItemStack stack = player.getStackInHand(hand);
+		NbtCompound nbt = stack.getNbt();
+		if (nbt != null && nbt.get("CustomModelData") != null) {
+			player.sendMessage(Text.of("You can't place that!"), true);
+			return ActionResult.FAIL;
+		}
+		return ActionResult.PASS;
+	}
 
+	@Override
+	public void onInitialize() {
+		UseBlockCallback.EVENT.register(this);
 	}
 }
