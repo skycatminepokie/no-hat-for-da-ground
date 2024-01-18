c794273384f77ebb3a61ee392fdb39731379c470
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-01-17 21:49:48 -0800

    Added MixinSquared, fixed client desync

--- src/main/java/com/skycat/nohatfordaground/NoHatForDaGround.java
+++ src/main/java/com/skycat/nohatfordaground/NoHatForDaGround.java
@@ -15,12 +15,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NoHatForDaGround implements ModInitializer, UseBlockCallback {
+	public static boolean sendUpdateAfterBlockBreak = false; // Used to send an inventory update packet when the server rejects the placement of a block
 	@Override
 	public ActionResult interact(PlayerEntity player, World world, Hand hand, BlockHitResult hitResult) {
 		ItemStack stack = player.getStackInHand(hand);
 		NbtCompound nbt = stack.getNbt();
 		if (nbt != null && nbt.get("CustomModelData") != null) {
 			player.sendMessage(Text.of("You can't place that!"), true);
+			sendUpdateAfterBlockBreak = true;
 			return ActionResult.FAIL;
 		}
 		return ActionResult.PASS;
