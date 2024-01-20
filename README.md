9cf2d2de753842e33f3744fa6471d8478cb5a88a
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-01-19 22:07:38 -0800

    Cherry-pick 0.0.3
    Annotate useless method as Deprecated lol
    
    (cherry picked from commit 48434070c714d32096da44f07926b0c716fc3e98)
    
    Fix README
    
    (cherry picked from commit d7a1b6814008ed92f3dfcf1410c835523612671a)
    
    Create README.md
    
    (cherry picked from commit 0a6cd1ce3c2415a8529a19ddf3ad6c28c3069bf7)
    
    Switch to much better one-mixin solution, version bump
    
    Props to Linguardium. One mixin, no Fabric API, no MixinSquared.
    
    (cherry picked from commit 51401518fd42934b1a93926c6430e3cdbcb9e748)

---
+++ README.md
@@ -0,0 +1,15 @@
+# Stop turning your hats into pumpkins!
+This pevents you from placing items with the `CustomModelData` NBT tag. All those fancy hats and stuff that you make out of carved pumpkins and resource packs won't be placed on the ground any more! It'll work server side, client side, or both.
+
+## For Server Owners
+When installed on **only** the server side, the items appear to be placed for a brief moment on the client side. Players will not get "lag back."
+
+Requires Fabric API for versions <= 0.0.2.
+
+## For Modpacks
+You can add this to your modpack as long as it's still hosted on Modrinth.
+
+## Questions and Contact
+Ports: Make an issue on [GitHub](https://github.com/skycatminepokie/no-hat-for-da-ground/issues) to request a version port. I won't port loaders.
+
+Questions? Either make an issue or ping `@skycatminepokie` on the Fabric Development Discord server. Enjoy!
---
+++ src/main/java/com/skycat/nohatfordaground/mixin/ItemPlacementContextMixin.java
@@ -0,0 +1,50 @@
+package com.skycat.nohatfordaground.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import org.jetbrains.annotations.Contract;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ItemPlacementContext.class)
+public abstract class ItemPlacementContextMixin extends ItemUsageContext {
+    /**
+     * Required by ItemUsageContext, but unused. Don't use it - mixins aren't meant to be constructed.
+     * @deprecated Don't use this.
+     */
+    @Contract("_,_,_->fail")
+    @Deprecated
+    private ItemPlacementContextMixin(PlayerEntity player, Hand hand, BlockHitResult hit) {
+        super(player, hand, hit);
+        throw new UnsupportedOperationException("Seriously, don't use this. Like bro, did you ignore the javadoc, the deprecated warning, and static analysis? You're asking for it man.");
+    }
+
+    @ModifyReturnValue(method = "canPlace", at = @At("RETURN"))
+    private boolean stopPlacingCustomModelData(boolean original) {
+        // Adapted from Linguardium's method: https://github.com/Linguardium/no-hat-for-da-ground/commit/33ae0e396bf29bd07edae919b6765e0569bd3a7a
+        // This is a lot less clean-looking, but it reduces it to one mixin and makes more sense in my head.
+        if (!original) return false; // If it wasn't going to place in the first place, don't bother.
+        ItemStack stack = this.getStack();
+        if (stack.getNbt() == null || !stack.getNbt().contains("CustomModelData", NbtElement.NUMBER_TYPE)) { // if it has no NBT or if it has no custom model data
+            return true; // Then we don't need to bother.
+        }
+        // We've decided to block it
+        PlayerEntity player = this.getPlayer();
+        if (player != null) { // If it was a player
+            this.getPlayer().sendMessage(Text.of("You can't place that!"), true); // Let the player know we're blocking it
+            if (player instanceof ServerPlayerEntity serverPlayer) { // If we're on the server side
+                serverPlayer.playerScreenHandler.syncState(); // Make sure the client gets the update
+            }
+        }
+        return false; // We decided we were going to block it earlier
+    }
+
+}
